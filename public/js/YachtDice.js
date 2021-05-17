b94ecde8e59e441c832bde3a6bcf3385849946f7
ideasidus <ideasidus@gmail.com>
2021-05-17 19:46:07 +0900

    인풋 받아서 같은 룸에 소켓 두개

---
+++ public/js/YachtDice.js
@@ -0,0 +1,278 @@
+// 코드 너무 스파게티
+var socket = io();
+var myName = ''
+var room = ''
+
+const diceElement = []; //HTML의 주사위 원소를 저장할 배열
+const rollButton = document.getElementById("rollButton"); //페이지의 주사위 굴리기 버튼
+const turnElement = document.getElementById("turn"); // 페이지에서 차례를 나타내는 부분
+const resetArea = document.getElementById("resetArea") // 리셋버튼이 나타나는 div
+const resetButton = document.createElement("button"); // 게임 종료후 승자와 게임 재시작 여부를 보여주는 원소
+
+//리셋 버튼의 스타일과 클릭시 호출할 함수를 정함
+resetButton.className = "btn btn-primary";
+resetButton.style.width = "30%";
+resetButton.style.height = "64px";
+// resetButton.addEventListener('click', resetGame);
+
+//처음 주사위 다섯개 변수를 초기화 하고 리스너를 설정한 뒤 값을 출력함
+for (let i = 0; i < 5; i++) {
+  diceElement[i] = document.getElementById("Dice"+i);
+  diceElement[i].addEventListener('click', function() {selectDice(i);});
+  diceElement[i].innerHTML = 0;
+}
+
+//페이지의 점수판을 가져와 변수로 저장
+const p1Element = [];
+const p2Element = [];
+for (let i = 0; i < 15; i++) {
+  p1Element[i] = document.getElementById(i+"-1");
+  p2Element[i] = document.getElementById(i+"-2");
+}
+// console.log(dice);
+
+
+// function overTurn() {
+//   p1Turn = !p1Turn;
+//   tempScore = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+//   for (let i = 0; i < 5; i++) {
+//     if (diceSelected[i])
+//     {
+//       selectDice(i);
+//     }
+//     diceElement[i].style.transform = "rotate(0deg)";
+//   }
+//   rollCount = 3;
+//   rollButton.innerHTML = "주사위 굴리기 (" + rollCount + "/3)"
+//   setListener();
+//   if (p1Turn){
+//     turnElement.innerHTML = "P1's Turn";
+//   }
+//   else {
+//     turnElement.innerHTML = "P2's Turn";
+//   }
+
+//   for (let i = 0; i < p1Element.length; i++) {
+//     if (p1Turn){
+//       p1Element[i].style.backgroundColor = "lightgreen";
+//       p2Element[i].style.backgroundColor = "#FEFCEF"
+//     }
+//     else {
+//       p1Element[i].style.backgroundColor = "#FEFCEF";
+//       p2Element[i].style.backgroundColor = "lightgreen"
+//     }
+//   }
+//   setListener();
+//   checkEnd();
+// }
+
+// function resetGame() {
+//   rollCount = 3;
+//   dice = [0, 0, 0, 0 ,0];
+//   diceSelected = [false,false,false,false,false];
+//   p1Turn = false;
+//   p1score = [,,,,,,0,0,,,,,,,0];
+//   p2score = [,,,,,,0,0,,,,,,,0];
+//   tempScore = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+//   turn = 0;
+
+//   resetArea.removeChild(resetButton);
+
+
+//   for (let i = 0; i < 15; i++) {
+//   p1Element[i].innerHTML = "";
+//   p2Element[i].innerHTML = "";
+//   p1Element[i].className = "scoreBoard-user p1";
+//   p2Element[i].className = "scoreBoard-user p2";
+//   }
+//   p1Element[6].innerHTML = "0/63";
+//   p2Element[6].innerHTML = "0/63";
+
+//   p1Element[6].className += " scoreSet";
+//   p1Element[7].className += " scoreSet";
+//   p1Element[14].className += " scoreSet";
+//   p2Element[6].className += " scoreSet";
+//   p2Element[7].className += " scoreSet";
+//   p2Element[14].className += " scoreSet";
+
+//   overTurn();
+// }
+
+function rollDice() {
+  socket.emit('rollDice');
+}
+
+//주사위의 선택 여부에 따라 변수의 값과 class를 바꾸는 함수
+function selectDice(dice_id){
+  socket.emit('selectDice', dice_id);
+}
+
+function setScore(event) {
+  var score_id = event.target.id.split('-');
+  score_id = score_id[0];
+  socket.to(socket.id).emit(setScore, score_id);
+  rollButton.removeEventListener('click', rollDice); // 주사위 굴리기를 눌렀을때 rollDice 함수가 불리도록 이벤트 리스너
+  if (socket.player == 1)
+  {
+    for(let i = 0; i < 15; i++) {
+      p1Element[i].removeEventListener('click', setScore);
+    }
+  }
+  else
+  {
+    for(let i = 0; i < 15; i++) {
+      p2Element[i].removeEventListener('click', setScore);
+    }
+  }
+}
+
+  //min 이상 max 미만을 반환하는 함수
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+  }
+
+
+// 소켓에 접속
+socket.on("gameFull", function (room) {
+  alert("방코드 " + room + " 은(는) 인원이 가득차 있습니다. 다른 코드를 입력해 주세요.");
+})
+
+socket.on("myData", function(data){
+  console.log("myData called");
+  myName = data.name;
+  room = data.room;
+  console.log(myName, room);
+})
+
+// 서버로부터의 메시지가 수신되면
+socket.on("chat message", function (data) {
+  $("#chat").append($('<li>').text(data));
+});
+
+socket.on('updateDice', function(dice, selectedDice, rollCount){
+  console.log(dice, selectedDice, rollCount);
+  for (let i = 0; i < 5; i++) {
+    if (!selectedDice[i]) {
+      diceElement[i].style.transform = "rotate(" + (getRandomInt(-60, 61)) + "deg)"
+    }
+    diceElement[i].innerHTML = dice[i];
+  }
+  rollButton.innerHTML = "주사위 굴리기 (" + rollCount + "/3)"
+});
+
+socket.on('selectDiceUpdate', function(selected, dice_id) {
+    if (selected) {
+      diceElement[dice_id].className="SelectedDice";
+      diceElement[dice_id].style.transform = "rotate(0deg)";
+    }
+    else
+    {
+      diceElement[dice_id].className="Dice";
+      diceElement[dice_id].style.transform = "rotate(" + getRandomInt(-60, 61) + "deg)";
+    }  
+});
+
+socket.on("showTempScore", function(score, tempScore, player){
+  if (player == 1) {
+    for (let i = 0; i < 6; i++) {
+      if(score[i] == undefined){
+        p1Element[i].innerHTML = tempScore[i];
+      }
+    }
+    for (let i = 8; i < 15; i++) {
+      if(score[i] == undefined){
+        p1Element[i].innerHTML = tempScore[i];
+      }
+    }
+  }
+  else{
+    for (let i = 0; i < 6; i++) {
+      if(score[i] == undefined){
+        p2Element[i].innerHTML = tempScore[i];
+      }
+    }
+    for (let i = 8; i < 15; i++) {
+      if(score[i] == undefined){
+        p2Element[i].innerHTML = tempScore[i];
+      }
+    }
+  }
+})
+
+socket.on('setListener', function(player, score){
+  rollButton.addEventListener('click', rollDice); // 주사위 굴리기를 눌렀을때 rollDice 함수가 불리도록 이벤트 리스너
+  if (player == 1) {
+    for(let i = 0; i < 15; i++) {
+      if (score[i] == undefined) {
+        p1Element[i].addEventListener('click', setScore);
+      }
+    }
+  }
+  else {
+    for(let i = 0; i < 15; i++) {
+      if (score[i] == undefined) {
+        p2Element[i].addEventListener('click', setScore);
+      }
+    }
+  }
+})
+
+socket.on('overTurn', function(otherplayer) {
+  for (let i = 0; i < 5; i++) {
+    diceElement[i].style.transform = "rotate(0deg)";
+  }
+  if (otherplayer == 2){
+    turnElement.innerHTML = "P1's Turn";
+  }
+  else {
+    turnElement.innerHTML = "P2's Turn";
+  }
+  socket.emit('overTurn', otherplayer);
+});
+
+socket.on('updateScore', function(score, player) {
+  if(player == 1){
+    for (let i = 0; i < 15; i++) {
+      if(score[i] == undefined)
+      {
+        p1Element[i].innerHTML = "";
+      }
+      else
+      {
+        p1Element[i].innerHTML = score[i];
+        p1Element[i].className += " scoreSet";    
+      }
+    }
+    p1Element[6].innerHTML += "/63";
+  }
+  else
+  {
+    for (let i = 0; i < 15; i++) {
+      if(score[i] == undefined)
+      {
+        p2Element[i].innerHTML = "";
+      }
+      else
+      {
+        p2Element[i].innerHTML = score[i];
+        p2Element[i].className += " scoreSet"; 
+      }
+    }
+    p2Element[6].innerHTML += "/63";
+  }
+})
+
+socket.on('highlight', function(player) {
+  for (let i = 0; i < p1Element.length; i++) {
+    if (player == 2){
+      p1Element[i].style.backgroundColor = "lightgreen";
+      p2Element[i].style.backgroundColor = "#FEFCEF"
+    }
+    else {
+      p1Element[i].style.backgroundColor = "#FEFCEF";
+      p2Element[i].style.backgroundColor = "lightgreen"
+    }
+  }
+})
---
+++ server.js
@@ -0,0 +1,259 @@
+const express = require('express');
+const app = new express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+var rooms = {};
+rooms['full'] = 2;
+rooms['empty'] = 0;
+
+var name;
+var room;
+var player = 0;
+
+// 3000번 포트에 개방
+server.listen(3000, function () {
+  console.log("Socket IO server listening on port 3000")
+});
+
+// 루트페이지 호출 시 join-room.html 반환 (이름과 방코드 입력)
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/join-room.html');
+});
+
+// 이름과 코드입력시 /game 호출하면, 게임 페이지 반환
+app.post('/game', function (req, res) {
+  name = req.body.name;
+  room = req.body.room;
+  if (rooms[room] >= 2) {
+    res.sendFile(__dirname + '/join-room.html');
+  }
+  else {
+    res.sendFile(__dirname + '/index.html');
+  }
+})
+
+io.on('connection', function (socket) {
+  var rollCount = 3; //주사위 던지는 횟수를 세는 변수
+  var dice = [0, 0, 0, 0, 0]; // 주사위 5개의 변수
+  var selectedDice = [false, false, false, false, false]; // 주사위의 선택 여부를 나타내는 변수
+  var score = [, , , , , , 0, 0, , , , , , , 0]; //p1의 점수판, 중간합계, 보너스, 총합은 선언되어 있고 나머지는 undefined
+  var tempScore = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; //주사위 눈에 따라 계산되는 임시 점수 변수
+  var turn = 0; //몇번 차례가 돌았는지 기록하는 변수
+  //기존 방에 재참여하여 인원수를 늘리는 것을 막는 코드.
+  console.log("Someone connect to socket");
+
+  socket.name = name;
+  socket.room = room;
+
+  socket.emit('myData', {
+    name: name,
+    room: room
+  });
+
+  console.log(name + " try join room " + room);
+
+  // room에 join시킨다. 인원수를 체크하고 새로운 방을 만들거나 기존방에 참가.
+  // 2명 이상일 시 따로 처리해야함.
+  if (rooms[room] == undefined) {
+    rooms[room] = 0;
+  }
+
+  socket.join(room);
+  rooms[room]++;
+  player = rooms[room];
+  socket.player = player;
+  console.log("room " + room + " 접속인원 " + rooms[room] + "(명)");
+  io.to(room).emit('chat message', "player " + player + ", " + name + " 이(가) 게임에 참여했습니다.");
+  console.log(name + " join room " + room);
+
+  if (rooms[room] == 2) {
+    io.to(room).emit('overTurn', player);
+  }
+  else {
+    
+  }
+
+  socket.on('gameData', function () {
+
+  })
+
+  socket.on('forceDisconnect', function () {
+    socket.disconnect();
+  });
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected: ' + socket.name);
+    socket.leave(socket.room);
+    if (room) {
+      rooms[room]--;
+    }
+  });
+
+  socket.on('rollDice', function () {
+    console.log("rollDice called");
+
+    if (rollCount > 0) {
+      for (let i = 0; i < 5; i++) {
+        if (!selectedDice[i]) {
+          dice[i] = getRandomInt(1, 7);
+        }
+      }
+      rollCount--;
+      calcScore();
+      socket.emit('updateDice', dice, selectedDice, rollCount);
+      socket.emit('showTempScore', score, tempScore, player);
+    }
+  });
+
+  socket.on('selectDice', function (dice_id) {
+    console.log("selectDice called");
+
+    if (rollCount < 3) {
+      selectedDice[dice_id] = !selectedDice[dice_id];
+      socket.emit('selectDiceUpdate', selectedDice[dice_id], dice_id);
+    }
+  })
+
+  socket.on('overTurn', function(otherplayer){
+    tempScore = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < 5; i++) {
+      selectedDice[i] = false;
+    }
+    rollCount = 3;
+    socket.emit('updateDice', dice, selectedDice, rollCount);
+    socket.emit('highlight', otherplayer);
+    if(otherplayer != socket.player)
+    {
+      console.log(socket.player);
+      io.to(socket.id).emit("setListener", player, score);
+    }
+    // checkEnd();
+  });
+
+  socket.on("setScore", function(score_id) {
+    score[score_id] = tempScore[score_id];
+    calcSum();
+    checkBonus();
+    socket.emit('updateScore', score, player);
+    socket.broadcast.to(room).emit('overTurn', score, player);
+  })
+
+  //min 이상 max 미만을 반환하는 함수
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+  }
+
+  //점수 계산하는 함수 tempScore에 값 저장
+  function calcScore() {
+    var diceCount = [0, 0, 0, 0, 0, 0];
+    var diceSum = 0;
+
+    for (var i = 0; i < 5; i++) {
+      diceCount[dice[i] - 1] = diceCount[dice[i] - 1] + 1;
+      diceSum += dice[i];
+    }
+
+    for (var i = 0; i < 6; i++) {
+      tempScore[i] = 0;
+      tempScore[i] = (i + 1) * diceCount[i];
+    }
+
+    //tempScore[6] Subtotal
+    //tempScore[7] Bonus
+
+    // Choice
+    tempScore[8] = 0;
+    tempScore[8] = diceSum;
+
+    // 4 of a kind
+    tempScore[9] = 0;
+    for (var i = 0; i < 6; i++) {
+      if (diceCount[i] >= 4) {
+        tempScore[9] = diceSum;
+        break;
+      }
+    }
+
+    // Full House
+    tempScore[10] = 0;
+    var fullHouseCheck = [0, 0];
+    for (var i = 0; i < 6; i++) {
+      if (diceCount[i] == 2) {
+        fullHouseCheck[0] = 1;
+      }
+      if (diceCount[i] == 3) {
+        fullHouseCheck[1] = 1;
+      }
+    }
+    if (fullHouseCheck[0] == 1 && fullHouseCheck[1] == 1) {
+      tempScore[10] = diceSum;
+    }
+
+    // S. Straight
+    tempScore[11] = 0;
+    if ((diceCount[0] > 0 && diceCount[1] > 0 && diceCount[2] > 0 && diceCount[3] > 0) ||
+      (diceCount[1] > 0 && diceCount[2] > 0 && diceCount[3] > 0 && diceCount[4] > 0) ||
+      (diceCount[2] > 0 && diceCount[3] > 0 && diceCount[4] > 0 && diceCount[5] > 0))
+      tempScore[11] = 15;
+
+    // L. Straight
+    tempScore[12] = 0;
+    if ((diceCount[0] > 0 && diceCount[1] > 0 && diceCount[2] > 0 && diceCount[3] > 0 && diceCount[4] > 0) ||
+      (diceCount[1] > 0 && diceCount[2] > 0 && diceCount[3] > 0 && diceCount[4] > 0 && diceCount[5] > 0))
+      tempScore[12] = 30;
+
+    // Yachoo
+    tempScore[13] = 0;
+    if (dice[0] == dice[1] && dice[1] == dice[2] && dice[2] == dice[3] && dice[3] == dice[4])
+      tempScore[13] = 50;
+  }
+
+  //게임 종료를 판별하는 함수
+  function checkEnd() {
+    turn++;
+    if (turn > 24) {
+      
+      if (p1score[14] > p2score[14]) {
+        resetButton.textContent = "[Player 1] 승리! 새 게임 시작";
+      }
+      else if (p1score[14] < p2score[14]) {
+        resetButton.textContent = "[Player 2] 승리! 새 게임 시작";
+      }
+      else {
+        resetButton.textContent = "비겼습니다! 새 게임 시작";
+      }
+      resetArea.appendChild(resetButton);
+    }
+  }
+
+  function calcSum() {
+    score[6] = 0;
+    score[7] = 0;
+    for (let i = 0; i < 6; i++) {
+      if (score[i] != undefined) {
+        score[6] += score[i];
+      }
+    }
+    score[14] = 0
+    for (let i = 6; i < 14; i++) {
+      if (score[i] != undefined) {
+        score[14] += score[i];
+      }
+    }
+  }
+
+  function checkBonus() {
+    if(score[6] >= 63)
+    {
+      score[7] = 35;
+    }
+  }
+
+});
